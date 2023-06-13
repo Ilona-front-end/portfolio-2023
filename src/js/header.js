317b976bb7a6a69569fc521cd4defbea0558a3a4
Ilona R <ilona.raugalaite@yahoo.co.uk>
2023-06-13 08:01:20 +0200

    add link to specific div
    close dropdown menu after links are clicked

--- src/js/header.js
+++ src/js/header.js
@@ -1,5 +1,6 @@
 const navBurgerMenuButton = document.getElementById('navBurgerMenuButton');
 const navDropdownContent = document.getElementById('navDropdownContent');
+const navDropdownLinks = document.querySelectorAll('#navDropdownContent a');
 const navCloseButton = document.getElementById('navCloseButton');
 
 navBurgerMenuButton.addEventListener('click', () => {
@@ -8,4 +9,10 @@ navBurgerMenuButton.addEventListener('click', () => {
 
 navCloseButton.addEventListener('click', () => {
   navDropdownContent.classList.toggle('hidden');
+});
+
+navDropdownLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navDropdownContent.classList.add('hidden');
+  });
 });
